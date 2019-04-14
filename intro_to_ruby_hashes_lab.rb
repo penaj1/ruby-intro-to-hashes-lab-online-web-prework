16c6e66e941a1cff40501ab6934375b15a846cdc
Janice Pena <jipena94@gmail.com>
2019-04-14 21:53:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,11 +21,11 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads =>{
     :pieces=>4,
-    :rent_in_dollars=>
+    :names=>
     {
       
     },
-    :names=>{
+    :rent_in_dollars=>{
     }
   }
 }
